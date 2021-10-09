ce0aa916b08f8bcf7aafae63073ad88089fa51a2
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-10-09 20:24:51 +0300

    new png img

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import './sass/main.scss';
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 1200px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
