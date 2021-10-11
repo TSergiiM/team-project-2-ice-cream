10b999372834d0d0b3fbd1faf59e8d473da1a5ff
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-10-11 22:27:45 +0300

    added modal

--- src/index.js
+++ src/index.js
@@ -29,3 +29,42 @@ import './sass/main.scss';
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+
+
+
+
+
+
+
+
+   
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-mobile]'),
+    closeModalBtn: document.querySelector('[data-modal-close-mobile]'),
+    modal: document.querySelector('[data-modal-mobile]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
