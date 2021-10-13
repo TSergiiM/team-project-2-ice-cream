29707e22dfaa020ca24617e0609d1c2e3f561442
Serhii Tatsakovych <serazgart@gmail.com>
2021-10-13 14:10:03 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -45,17 +45,3 @@ import './sass/main.scss';
   }
 })();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-mobile]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
